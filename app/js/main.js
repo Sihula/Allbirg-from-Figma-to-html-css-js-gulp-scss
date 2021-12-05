ddc9381226e51d82d1ef64fe47bb216cb5f9f9dc
Sihula <serhiisihula@gmail.com>
2021-12-05 15:46:16 +0100

    add footer and adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,30 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+          autoplay: true,
+        }
+      },
+
+
+    ]
   });
 
   $('.slider__arrivals-items').slick({
@@ -20,7 +43,30 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+          autoplay: true,
+        }
+      },
+
+
+    ]
   });
 
   $('.slider__stories-items').slick({
@@ -28,7 +74,43 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>',
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false,
+          arrows: false,
+          autoplay: true,
+        }
+      },
+
+
+    ]
+
   });
 
 });
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger__btn');
+  const nav = document.querySelector('.nav');
+
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('burger__active');
+  });
+};
+
+navSlide();
