ecbff42fe805803cb39cb8f6a2e48f0a0515cd05
Sihula <serhiisihula@gmail.com>
2021-12-05 14:12:28 +0100

    add all sections

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,28 @@ $(function () {
 
   });
 
+  $('.slider__collection-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>'
+  });
 
+  $('.slider__arrivals-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>'
+  });
+
+  $('.slider__stories-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt="prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt="next"></button>'
+  });
 
 });
